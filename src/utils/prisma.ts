5b01e666bd2ba267200659e675b8b49032058d6b
PhelipeG <luisphelipe1000@gmail.com>
2025-10-06 02:11:28 -0300

    feat:configuraçao robusta do prisma

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,8 +1,22 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient({
-  log:
-    process.env.NODE_ENV === 'development'
-      ? ['query', 'error', 'warn']
-      : ['error'],
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['query', 'error', 'warn'],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+prisma.$connect().catch((error) => {
+  console.error('❌ Erro ao conectar com MongoDB:', error);
+  process.exit(1);
 });
